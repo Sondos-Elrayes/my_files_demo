f3933d5091630609b890e041af149dff5718ba67
sondos <elrayessondos9@gmail.com>
2021-04-04 21:30:06 +0200

    flutter project

--- lib/my_files_screen.dart
+++ lib/my_files_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_files/my_files_containers.dart';
+import 'my_files_containers.dart';
 
 class MyFilesScreen extends StatefulWidget {
   @override
